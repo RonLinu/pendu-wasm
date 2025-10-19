e4a51862371f637756a7a662b7650ab6a2e29218
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-19 15:42:46 -0400

    Delete pendu/pendu_aide.js

--- pendu/pendu_aide.js
+++
@@ -1,5 +0,0 @@
-Ce jeu bien connu consiste à deviner, une lettre à la fois, un mot caché qui est choisi au hazard parmi plus de 17000 mots français.
-<br><br>
-Si vous accumulez 10 échecs sans avoir trouvé un mot caché, vous perdez \uD83D\uDE1E
-<br><br>
-Cliquer sur une lettre dévoile aussi toutes les lettres accentuées correspondantes.
